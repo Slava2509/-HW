5211a1d5b8d637cdc35252050044f75a5899c585
geekbrains <info@geekbrains>
2022-12-08 00:21:05 +0500

    пересоздали репозиторий

---
+++ Homework1/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7->max = 7
+// a = 2 b = 10->max = 10
+// a = -9 b = -3->max = -3
+
+// Решение:
+
+// Console.WriteLine("Введите первое число ");
+// int numberA = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число ");
+// int numberB = int.Parse(Console.ReadLine()!);
+// int max = numberA;
+// int min = numberB;
+// if (numberA < numberB)
+// {
+//     max = numberB;
+//     min = numberA;
+// }
+// Console.Write("Максимальное число ");
+// Console.Write(max);
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Решение:
+
+// Console.WriteLine("Введите первое число ");
+// int numberA = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число ");
+// int numberB = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите третье число ");
+// int numberC = int.Parse(Console.ReadLine()!);
+// int max = numberA;
+// if (numberA < numberB)
+// {
+//     max = numberB;
+// }
+// if (numberB < numberC & numberA < numberC)
+// {
+//     max = numberC;
+// }
+// Console.Write("Максимальное число:  ");
+// Console.Write(max);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4->да
+// - 3->нет
+// 7->нет
+
+// Решение:
+
+// Console.WriteLine("Введите число ");
+// int numberA = int.Parse(Console.ReadLine()!);
+// if (numberA % 2 == 0)
+// {
+//     Console.WriteLine("число четное ");
+// }
+// if (numberA % 2 != 0)
+// {
+//     Console.WriteLine("число является нечетным ");
+// }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Решение:
+
+// Console.WriteLine("Введите число от 1");
+// int N = int.Parse(Console.ReadLine()!);
+// int i = 2;
+// while (N >= i)
+// {
+//     Console.Write(i + ",");
+//     i = i + 2;
+// }
+
---
+++ Homework2/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//Решение:
+
+//int num = new Random().Next(100, 999); (Если лень вводить)
+// Console.WriteLine("Введите трехзначное число ");
+// int num = int.Parse(Console.ReadLine()!);
+// if (num < 100 || num > 999)
+// {
+//     Console.WriteLine("Вы ввели число не соответствующее условию");
+//     return;
+// }
+// int num2 = (num / 10) % 10;
+// Console.WriteLine(num2);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+//Решение:
+
+// Console.WriteLine("Введите любо число");
+// int num = int.Parse(Console.ReadLine()!);
+// int index = 0;
+// if (num < 100)
+// {
+//     Console.WriteLine("третьей цифры нет");
+// }
+// while (num > 999)
+// {
+//     num = num / 10;
+//     index = index + 1;
+//     if (num < 999)
+//     {
+//         num = num % 10;
+//     }
+// }
+// Console.WriteLine(num);
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// //Решение:
+
+
+// Console.Write("Введите число от 1 до 7: ");
+// int numberDay = int.Parse(Console.ReadLine()!);
+// if (numberDay < 1 || numberDay > 7)
+// {
+//     Console.WriteLine("Введено не корректное число");
+// }
+// if (numberDay == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// if (numberDay == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// if (numberDay == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// if (numberDay == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// if (numberDay == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// if (numberDay == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// if (numberDay == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// if (numberDay < 6 & numberDay > 1)
+// {
+//     Console.Write("не является выходным, т.к. является будничным днем");
+// }
+// if (numberDay == 6 || numberDay == 7)
+// {
+//     Console.Write("ура!!! выходной");
+// }
