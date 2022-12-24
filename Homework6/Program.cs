899cc94641dbdce1f5a68d2765eb6db2ea97f375
geekbrains <info@geekbrains>
2022-12-24 19:44:18 +0500

    HW7

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.Clear();
-// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+﻿// Console.Clear();
+// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// // 0, 7, 8, -2, -2 -> 2
+// // 1, -7, 567, 89, 223-> 3
 
 // Console.WriteLine("Введите количество элементов массива: ");
 // int m = int.Parse(Console.ReadLine()!);
@@ -19,6 +19,7 @@
 //     return array;
 // }
 // Console.WriteLine(CreateArr(array));
+
 // for (int i = 0; i < array.Length; i++)
 // {
 
@@ -36,16 +37,16 @@
 //значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
 
-Console.WriteLine("введите число b1");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("введите число k1");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("введите число b2");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("введите число k2");
-double k2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("введите число b1");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("введите число b2");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("введите число k2");
+// double k2 = Convert.ToDouble(Console.ReadLine());
 
-double x = (-b2 + b1) / (-k1 + k2);
-double y = k2 * x + b2;
+// double x = (-b2 + b1) / (-k1 + k2);
+// double y = k2 * x + b2;
 
-Console.WriteLine($"точка пересечения двух прямых будет в точке с координатами X: {x}, Y: {y}");
+// Console.WriteLine($"точка пересечения двух прямых будет в точке с координатами X: {x}, Y: {y}");
